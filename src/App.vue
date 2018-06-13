0987d54dce5b1f89825caf74b7c8c3cc745a9563
Andrew Yoon <ayoon212@gmail.com>
2018-06-13 15:09:45 -0700

    Basic markup and background

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- Header? -->
+    <!-- Background "NBB" -->
+    <Home />
+    <!-- Nav -->
+    <!-- Footer -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue'
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    Home
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+html, body {
+  margin: 0;
+  padding: 0;
+}
+body {
+  background-image: url("assets/background/black-Linen.png");
 }
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="home">
+    <h1>Hero image goes here</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style lang="scss">
+#home {
+  color: #fff;
+  width: 80%;
+  margin: 0 auto;
+}
+</style>
